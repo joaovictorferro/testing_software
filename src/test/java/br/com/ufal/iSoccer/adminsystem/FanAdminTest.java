0627168cf39620047b901520900514fadba74087
João Victor Ribeiro Ferro <jvrf@ic.ufal.br>
2021-01-15 16:18:18 -0300

    up

---
+++ src/test/java/br/com/ufal/iSoccer/adminsystem/FanAdminTest.java
@@ -0,0 +1,11 @@
+package br.com.ufal.iSoccer.adminsystem;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FanAdminTest {
+    @Test
+    public void creatFan(){
+        Assert.assertTrue(true);
+    }
+}
